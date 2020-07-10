b13303ee346418bc1a3d4cc7d43953c14cfc271d
Rengf <1032387871@qq.com>
2020-07-10 20:38:56 +0800

    first

--- models/category.js
+++ models/category.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/myblog", {
+mongoose.connect("mongodb://127.0.0.1:27017/myblog", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
